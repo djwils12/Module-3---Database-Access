1bc35ea05107f173befd72080b5781dfd931f22a
Dave <wondislave@gmail.com>
2023-02-27 00:57:16 -0600

    completed assignment

---
+++ config/db.manager.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+const dbConnection = require('./db.config');
+console.log(dbConnection);
+async function getCustomers(){
+console.log(' Connecting to SQL....... Cloud Server');
+let dbContext = await sql.connect(dbConnection);
+console.log('The Databse connection was Successful');
+console.log('Getting data');
+let results = await dbContext.request()
+.query(`
+SELECT TOP(20)
+CustomerID,
+NameStyle,
+Title,
+FirstName,
+MiddleName,
+LastName
+FROM
+salesLT.Customer
+`);
+console.log(`Returned SQL results`);
+return results;
+}
+//Export
+module.exports = {getCustomers :getCustomers};
